9156b22f7cae861a939e352f08df1ba24b563088
Emir Kandemir <emirkagankandemir@gmail.com>
2023-10-29 12:19:35 +0300

    Initial Commit

---
+++ LinuxCommandApp/LinuxCommandAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinuxCommandAppApp.swift
+//  LinuxCommandApp
+//
+//  Created by Emir Kandemir on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LinuxCommandAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
